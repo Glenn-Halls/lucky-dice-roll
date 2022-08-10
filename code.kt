51d65c2ce1b5bd0594e4753b4a9ed2637f09d564
Glenn Halls <gjhalls@gmail.com>
2022-08-10 13:47:41 +1000

    Working: conditional behavior in Kotlin

--- code.kt
+++ code.kt
@@ -1,10 +1,26 @@
-fun main() {
-   val num = 6
-   if (num > 4) {
-       println("The variable is greater than 4")
-   } else if (num == 4) {
-        println("The number is 4")
-   } else {
-        println("The variable is less than 4")
-   }
+fun main () {
+     val myFirstDie = Die(6)
+     val rollResult = myFirstDie.roll()
+     val luckyNumber = 4
+     
+     if (rollResult == luckyNumber) {
+          println("You Win!!!")
+     } else if (rollResult == 1) {
+          println("You rolled a 1, try again!")
+     } else if (rollResult == 2) {
+          println("Argh, you rolled a 2, try again!")
+     } else if (rollResult == 3) {
+          println("You rolled a 3, have another crack!")
+     } else if (rollResult == 5) {
+          println("So close, you rolled a 5, try again!")
+     } else {
+          println("You didn't win, you rolled a 6, please try again!")
+     }
+}
+
+class Die (val numSides: Int) {
+     
+     fun roll(): Int {
+          return (1..numSides).random()
+     }
 }

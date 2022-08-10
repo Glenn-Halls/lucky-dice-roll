6b596164c55fe928f1fd74de0c0e38238650b6fe
Glenn Halls <gjhalls@gmail.com>
2022-08-10 14:06:46 +1000

    Finished: if, else, when

--- code.kt
+++ code.kt
@@ -3,23 +3,18 @@ fun main () {
      val rollResult = myFirstDie.roll()
      val luckyNumber = 4
      
-     if (rollResult == luckyNumber) {
-          println("You Win!!!")
-     } else if (rollResult == 1) {
-          println("You rolled a 1, try again!")
-     } else if (rollResult == 2) {
-          println("Argh, you rolled a 2, try again!")
-     } else if (rollResult == 3) {
-          println("You rolled a 3, have another crack!")
-     } else if (rollResult == 5) {
-          println("So close, you rolled a 5, try again!")
-     } else {
-          println("You didn't win, you rolled a 6, please try again!")
+     when (rollResult) {
+          luckyNumber -> println("You Win!!!")
+          1 -> println("1")
+          2 -> println("2")
+          3 -> println("3")
+          5 -> println("5")
+          6 -> println("6")
+          else -> println("Wow, you are not using a 6 sided die, get outta here!!!")
      }
 }
 
 class Die (val numSides: Int) {
-     
      fun roll(): Int {
           return (1..numSides).random()
      }

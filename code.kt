60225db0d553c4a5bac1060e41ece0a6360a8689
Glenn Halls <gjhalls@gmail.com>
2022-08-10 13:26:47 +1000

    Initial commit

---
+++ code.kt
@@ -0,0 +1,10 @@
+fun main() {
+   val num = 6
+   if (num > 4) {
+       println("The variable is greater than 4")
+   } else if (num == 4) {
+        println("The number is 4")
+   } else {
+        println("The variable is less than 4")
+   }
+}
